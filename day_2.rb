be2c2eeb5ad94a9b63acce20442107915ca9909e
Kevin Finn <superkevin@gmail.com>
2021-12-13 16:20:21 -0500

    part-2

--- day_2.rb
+++ day_2.rb
@@ -3,17 +3,20 @@ require 'active_support/all'
 File.open('day_2_input.txt') do |file|
   horizontal_position = 0
   depth = 0
+  aim = 0
 
   file.each_line do |line|
     direction, distance_string = line.split(' ')
     distance = distance_string.to_i
+    
     case direction
     when 'forward'
       horizontal_position += distance
+      depth += distance * aim
     when 'down'
-      depth += distance
+      aim += distance
     when 'up'
-      depth -= distance
+      aim -= distance
     else
       raise "invalid instruction: #{line}"
     end
