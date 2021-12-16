e307275fac6a5db7180d1b2559bc05687eff5466
Kevin Finn <superkevin@gmail.com>
2021-12-16 16:28:33 -0500

    part-2

--- day_8.rb
+++ day_8.rb
@@ -3,6 +3,8 @@ require './line'
 
 File.open("day_8_input.txt") do |file|
 # File.open("day_8_input_small.txt") do |file|
-  lines = file.each_line.map { |line| Line.new(line.each_char.to_a) }
+  lines = file.each_line.compact_blank.map { |line| Line.new(line.squish.each_char.to_a) }
   puts lines.select(&:corrupted?).sum(&:score)
+  incomplete_lines = lines.select(&:incomplete?)
+  puts incomplete_lines.map(&:score).sort[incomplete_lines.size / 2]
 end
--- line.rb
+++ line.rb
@@ -13,6 +13,13 @@ class Line
     '>' => 25137
   }
 
+  SCORES_BY_AUTOCOMPLETE_SUGGESTION_CHARACTER = {
+    ')' => 1,
+    ']' => 2,
+    '}' => 3,
+    '>' => 4
+  }
+
   def initialize(char_array)
     @char_array = char_array
   end
@@ -23,26 +30,53 @@ class Line
     first_corrupted_character.present?
   end
 
-  def score
-    SCORES_BY_FIRST_CORRUPTED_CHARACTER.fetch(first_corrupted_character, 0)
+  def incomplete?
+    autocomplete_suggestion.any?
   end
 
+  def score
+    if corrupted?
+      SCORES_BY_FIRST_CORRUPTED_CHARACTER.fetch(first_corrupted_character, 0)
+    else
+      autocomplete_suggestion.reduce(0) do |partial_score, char|
+        (partial_score * 5) + SCORES_BY_AUTOCOMPLETE_SUGGESTION_CHARACTER[char]
+      end
+    end
+  end
 
   def first_corrupted_character
-    unless instance_variable_defined?(:@first_corrupted_character)
-      open_characters = []
-      char_array.each do |char|
-        if CLOSING_CHARACTERS_BY_OPENING_CHARACTER.include? char
-          open_characters.push char
-        elsif char == CLOSING_CHARACTERS_BY_OPENING_CHARACTER[open_characters.last]
-          open_characters.pop
-        else
-          @first_corrupted_character = char
-          break
-        end
-      end
-      @first_corrupted_character ||= nil
-    end
+    process_if_necessary!
     @first_corrupted_character
+  end
+
+  def autocomplete_suggestion
+    process_if_necessary!
+    @autocomplete_suggestion
+  end
+
+  def process_if_necessary!
+    return if instance_variable_defined?(:@processed)
+
+    @first_corrupted_character = nil
+
+    open_characters = []
+    char_array.each do |char|
+      if CLOSING_CHARACTERS_BY_OPENING_CHARACTER.include? char
+        open_characters.push char
+      elsif char == CLOSING_CHARACTERS_BY_OPENING_CHARACTER[open_characters.last]
+        open_characters.pop
+      else
+        @first_corrupted_character = char
+        break
+      end
+    end
+
+    if @first_corrupted_character.nil?
+      @autocomplete_suggestion = open_characters.reverse.map { |char| CLOSING_CHARACTERS_BY_OPENING_CHARACTER[char] }
+    else
+      @autocomplete_suggestion = []
+    end
+
+    @processed = true
   end
 end
