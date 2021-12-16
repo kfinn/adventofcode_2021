34be4cdeb3afab8cbb12a78e5cfc7fabb996c457
Kevin Finn <superkevin@gmail.com>
2021-12-16 15:42:11 -0500

    part-2

--- day_6.rb
+++ day_6.rb
@@ -8,4 +8,9 @@ File.open('day_6_input.txt') do |file|
   80.times { fish_age_distribution = fish_age_distribution.succ }
   puts fish_age_distribution.fish_counts_by_age
   puts fish_age_distribution.total_fish_count
+
+  fish_age_distribution = initial_fish_age_distribution
+  256.times { fish_age_distribution = fish_age_distribution.succ }
+  puts fish_age_distribution.fish_counts_by_age
+  puts fish_age_distribution.total_fish_count
 end
