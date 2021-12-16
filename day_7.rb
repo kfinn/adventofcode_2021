1ed094858dd3d920a10394566f98b1d55a44b8dc
Kevin Finn <superkevin@gmail.com>
2021-12-16 16:00:36 -0500

    part-1

---
+++ day_7.rb
@@ -0,0 +1,22 @@
+require 'active_support/all'
+
+File.open("day_7_input.txt") do |file|
+# File.open("day_7_input_small.txt") do |file|
+  starting_crab_positions = file.readline.split(",").map(&:to_i)
+  sorted_starting_crab_positions = starting_crab_positions.sort
+  
+  median = sorted_starting_crab_positions[starting_crab_positions.size / 2]
+  mean = starting_crab_positions.sum / starting_crab_positions.size
+  
+  fuel_cost = lambda do |target|
+    starting_crab_positions.sum do |crab_position|
+      (target - crab_position).abs
+    end
+  end
+
+  puts "median: #{median - 1}; cost: #{fuel_cost.call(median - 1)}"
+  puts "median: #{median}; cost: #{fuel_cost.call(median)}"
+  puts "median + 1: #{median + 1}; cost: #{fuel_cost.call(median + 1)}"
+  puts "median + 10: #{median + 10}; cost: #{fuel_cost.call(median + 10)}"
+  puts "mean: #{mean}; cost: #{fuel_cost.call(mean)}"
+end
